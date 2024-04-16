5dbfc19157f2d5059a8ed34347a8c7c97933b8d2
Ryan Agius <ryan@smartcow.ai>
2024-04-16 21:17:12 +0200

    first app example
    Signed-off-by: Ryan Agius <ryan@smartcow.ai>

---
+++ first-app/src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HousingLocationComponent
+  ],
+  template: `
+  <section>
+    <form>
+      <input type="text" placeholder="Filter by city">
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section>
+`,
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
